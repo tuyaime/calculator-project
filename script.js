b7c6c825103dc8b52dc6a4acd2785afb2c001d79
TUYISENGE Emerthe <tuyisengeemerthe@gmail.com>
2021-09-04 21:11:19 +0200

    Add id & js

---
+++ script.js
@@ -0,0 +1,35 @@
+let input = document.getElementById("inputEnter")
+
+let button = document.getElementById("btnPlus")
+
+let btnMinus = document.getElementById("btnMinus")
+
+let btnTimes = document.getElementById("btnTimes")
+
+let btnDivid = document.getElementById("btnDivid")
+
+let btn7 = document.getElementById("btn7")
+
+let btn8 = document.getElementById("btn8")
+
+let btn9 = document.getElementById("btn9")
+
+let btnEqual = document.getElementById("btnEqual")
+
+let btn4 = document.getElementById("btn4")
+
+let btn5 = document.getElementById("btn5")
+
+let btn6 = document.getElementById("btn6")
+
+let btn1 = document.getElementById("btn1")
+
+let btn2 = document.getElementById("btn2")
+
+let btn3 = document.getElementById("btn3")
+
+let btn0 = document.getElementById("btn0")
+
+let btnDot = document.getElementById("btnDot")
+
+let btnClose = document.getElementById("btnClose")
